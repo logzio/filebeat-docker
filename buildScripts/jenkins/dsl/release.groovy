a3957d45a772a9e4be382c3fd27d3d5453583415
Moshe Eshel <moshe.eshel@kenshoo.com>
2017-02-19 18:52:05 +0200

    fix dsl

--- buildScripts/jenkins/dsl/release.groovy
+++ buildScripts/jenkins/dsl/release.groovy
@@ -1,10 +1,6 @@
-def NAME = "connect-backend"
+def NAME = "filebeat-docker"
 def BRANCH_NAME = "master"
-def EMAIL = "_DevReporting@kenshoo.com"
-def STAGING_PLACEMENT = "labs_east"
-def PROD_PLACEMENT = "prod_east"
-def STAGING_LABEL = "staging"
-def PROD_LABEL = "prod"
+def EMAIL = "_DevMicrocosm@kenshoo.com"
 def JOB_NAME = "${NAME}-release"
 def DOCKER_URL = "kenshoo-docker.jfrog.io/$NAME"
 
@@ -40,20 +36,6 @@ job(JOB_NAME) {
         }
     }
 
-    configure { project ->
-        project / 'publishers' << 'net.masterthought.jenkins.CucumberReportPublisher' {
-            jsonReportDirectory ""
-            fileIncludePattern '**/cucumber.json'
-            fileExcludePattern ""
-            skippedFails false
-            pendingFails false
-            undefinedFails false
-            missingFails false
-            ignoreFailedTests false
-            parallelTesting false
-        }
-    }
-
 
 
     wrappers {
@@ -74,61 +56,15 @@ job(JOB_NAME) {
     }
 
     steps {
-        shell("""
-            virtualenv venv
-            source venv/bin/activate
-            
-            pip install microcosm-cli --extra-index-url="https://jenkins:\${AWS_ARTIFACTORY_PASS}@artifactory.kenshoo-lab.com/artifactory/api/pypi/PyPI-releaes/simple/" --trusted-host artifactory.kenshoo-lab.com
-            micro init --token xxxxxxxx
-            
-            #micro validate_structure            
-        """)
-
-        gradle {
-            useWrapper(false)
-            tasks('clean build')
-            switches("-PbuildNum=$BUILD_NUMBER ")
-        }
-        
         shell("""            
             docker login -u microcosm -p \$ARTIFACTORY_MICROCOSM_PASSWORD kenshoo-docker.jfrog.io        
             docker build -t  $DOCKER_URL:\$BUILD_NUMBER .
             docker push $DOCKER_URL:\$BUILD_NUMBER            
         """)
 
-        downstreamParameterized {
-            trigger('connect-backend-lab-deploy') {
-                parameters {
-                    predefinedProp('DOCKER_IMAGE_BUILD', "\${BUILD_NUMBER}")
-                }
-            }
-        }
-
-        shell("""
-            source venv/bin/activate
-
-            micro version --placement ${STAGING_PLACEMENT} --app ${NAME} --label ${STAGING_LABEL} --version \${BUILD_NUMBER} --yes            
-            micro status --placement ${STAGING_PLACEMENT} --app ${NAME} --label ${STAGING_LABEL} --wait_for READY        
-            
-        """)
-        gradle {
-            useWrapper(false)
-            tasks('smoketest')
-            switches("-Dsmoke.protocol=https -Dsmoke.host=${NAME}-${STAGING_LABEL}.kenshoo-lab.com -Dsmoke.port=443")
-        }
-        
-        shell("""
-            source venv/bin/activate
-
-            micro version --placement ${PROD_PLACEMENT} --app ${NAME} --label ${PROD_LABEL} --version \${BUILD_NUMBER} --yes            
-            micro status --placement ${PROD_PLACEMENT} --app ${NAME} --label ${PROD_LABEL} --wait_for READY        
-            
-        """)
-
     }
 
     publishers {
-        archiveJunit('**/test-results/TEST-*.xml, **/test-results/*/TEST-*.xml, **/test-results.xml')
         extendedEmail("${EMAIL}") {
             trigger(triggerName: 'Unstable',
                     sendToDevelopers: true, sendToRequester: true, includeCulprits: true, sendToRecipientList: false)
